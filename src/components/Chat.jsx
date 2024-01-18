673ee8c0817afbfb29361a7fe6c80754abec2e5c
Rowan Molony <rowanmolony@gmail.com>
2024-01-18 09:28:40 +0000

    add Chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default function Chat({ }) {
   return (
-    <div className="text-center">
-      <div>
+    <div className="rounded-xl shadow-lg text-center bg-gray-100">
+      <div className="py-4">
         <p>Dia Duit, cad faoi ar mhaith labhairt?</p>
       </div>
-      <input></input>
+      <div>
+        <input className="rounded-xl shadow-lg p-2 mt-2 mb-4 "></input>
+      </div>
     </div>
   )
 }
