0157921a64bf1617ed470d2580eda9ce4d7b84cb
Rowan Molony <rowanmolony@gmail.com>
2024-01-19 10:14:38 +0000

    wip

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Chat({ name }) {
   return (
-    <div className="rounded-xl shadow-lg text-center bg-gray-100 m-4">
+    <div className="rounded-xl shadow-lg text-center m-12">
       <div className="py-4">
         <p>Dia Duit <strong>{name}</strong>, cad faoi ar mhaith labhairt?</p>
       </div>
       <div>
-        <input className="rounded-xl shadow-lg p-2 mt-2 mb-4 "></input>
+        <input className="rounded-xl shadow-lg p-2 mt-2 mb-4 mx-12 bg-slate-50"></input>
       </div>
     </div>
   )
 }
+
+Chat.propTypes = {
+  /** Name of person to greet */
+  name: PropTypes.string.isRequired,
+};
--- src/gen/Chat.js
+++ src/gen/Chat.js
@@ -5,14 +5,19 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = Chat;
 var _react = _interopRequireDefault(require("react"));
+var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function Chat(_ref) {
   var name = _ref.name;
   return /*#__PURE__*/_react["default"].createElement("div", {
-    className: "rounded-xl shadow-lg text-center bg-gray-100 m-4"
+    className: "rounded-xl shadow-lg text-center m-12"
   }, /*#__PURE__*/_react["default"].createElement("div", {
     className: "py-4"
   }, /*#__PURE__*/_react["default"].createElement("p", null, "Dia Duit ", /*#__PURE__*/_react["default"].createElement("strong", null, name), ", cad faoi ar mhaith labhairt?")), /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("input", {
-    className: "rounded-xl shadow-lg p-2 mt-2 mb-4 "
+    className: "rounded-xl shadow-lg p-2 mt-2 mb-4 mx-12 bg-slate-50"
   })));
 }
+Chat.propTypes = {
+  /** Name of person to greet */
+  name: _propTypes["default"].string.isRequired
+};
